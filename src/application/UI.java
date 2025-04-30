505eddd7ac722a885d54e352284eb00e51976899
Diego Reis <diegoreso@me.com>
2025-04-30 15:57:26 -0300

    update left king position

--- src/application/UI.java
+++ src/application/UI.java
@@ -54,12 +54,12 @@ public class UI {
             }
             System.out.println();
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println("  A B C D E F G H");
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print(( 8 - i )+ " ");
+            System.out.print(( 8 - i )+ "  ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j], possibleMoves[i][j]);
             }
